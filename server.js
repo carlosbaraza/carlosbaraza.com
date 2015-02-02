e397c1136a91e28c8f8813675aa4bc635e2c72a0
Carlos Baraza Haro <carlos.baraza@kneip.com>
2015-02-02 18:31:36 +0100

    Change template to Nodebootstrap kick off template

---
+++ server.js
@@ -0,0 +1,9 @@
+require('nodebootstrap-server').setup(function(runningApp) {
+
+  //---- Mounting well-encapsulated application modules
+  //---- See: http://vimeo.com/56166857
+
+  runningApp.use('/hello', require('hello')); // attach to sub-route
+  runningApp.use(require('routes')); // attach to root route
+
+});
