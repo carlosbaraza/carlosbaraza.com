7630ac029b05d7de07075ed6c872a0e415e5dc37
Carlos Baraza Haro <carlos.baraza@kneip.com>
2015-02-10 18:26:11 +0100

    Add ugly helper for the asynchronous tests with superagent.js

--- test/acceptance/terminal_spec.js
+++ test/acceptance/terminal_spec.js
@@ -1,10 +1,22 @@
 // var superagent = require("superagent");
 
+// Ugly helper for asynchronous tests
+function check( done, f ) {
+  try {
+    f()
+    done()
+  } catch( e ) {
+    done( e )
+  }
+}
+
 describe('Terminal', function() {
-  it('loads the index page', function() {
+  it('loads the index page', function(done) {
     superagent.get('http://localhost:3000/')
               .end(function(data, textStatus, xhr) {
-      expect(data.status).to.equal(200);
+      check(done, function() {
+        expect(data.status).to.equal(200);
+      });
     });
   });
 });
